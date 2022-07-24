29ce3a20095579e646af4d21a9212e28fa94573a
Kacper Lewicki <kacper.lewicki@onet.pl>
2022-07-24 17:58:41 +0200

    end-project-saper

---
+++ JS/ResetButton.js
@@ -0,0 +1,12 @@
+import { UI } from "./UI.js";
+
+
+export class resetButton extends UI {
+    element = this.getElement(this.UiSelectors.resetButton);
+
+    changeEmotion(emotion) {
+      this.element
+        .querySelector('use')
+        .setAttribute('href', `./assets/sprite.svg#${emotion}`);
+    }
+  }
